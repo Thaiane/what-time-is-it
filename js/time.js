d407c1fd7afd24d6824c0ee90df79ae9a00cda64
Thaiane Braga <Thaiane_Braga@gap.com>
2020-08-30 19:42:59 -0300

    Agora essa droga foi

--- js/time.js
+++ js/time.js
@@ -1,6 +1,1 @@
-export const timeSF = new Date()
-
-export const timePOA = "14h00"
-
-export const tempoSF = "34 graus"
-export const tempoPOA = "24 graus"
+export document.getElementById("demo").innerHtml = "Hello JavaScript!";
