7d1cf1ed3ecf626972731e99f3210be07f15f75a
Thaiane Braga <Thaiane_Braga@gap.com>
2020-08-30 23:05:36 -0300

    Com hora em poa e sf

--- js/time.js
+++ js/time.js
@@ -1,4 +1,9 @@
-function myFunction() {
+function timeSF() {
     let time = new Date();
-    return document.getElementById("demo").innerHTML = "Hora " + time;
+    return document.getElementById("timeSF").innerHTML = "Hora " + (time.getHours() - 4) + "h" + time.getMinutes();
+}
+
+function timePOA() {
+    let time = new Date();
+    return document.getElementById("timePOA").innerHTML = "Hora " + time.getHours() + "h" + time.getMinutes();
 }
