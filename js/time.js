383ced633a55840b615f17b13fbf439c238761a2
Thaiane Braga <Thaiane_Braga@gap.com>
2020-08-31 21:31:22 -0300

    Exemplo de tempo corrente

--- js/time.js
+++ js/time.js
@@ -3,12 +3,36 @@ function timeSF() {
     let min = new Date().getMinutes();
     let minFormatt = ("00" + min).slice(-2);
 
-    return document.getElementById("timeSF").innerHTML = "Hora " + hour + "h" + minFormatt;
+    document.getElementById("timeSF").innerHTML = "Hora " + hour + "h" + minFormatt;
 }
 
 function timePOA() {
     let hour = new Date().getHours();
     let min = new Date().getMinutes();
     let minFormatt = ("00" + min).slice(-2);
+
     return document.getElementById("timePOA").innerHTML = "Hora " + hour + "h" + minFormatt;
 }
+
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+    var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+currentTime();
