1f8aa812838c851165473fdb1c80a7dfad5d01da
Thaiane Braga <Thaiane_Braga@gap.com>
2020-09-02 11:45:50 -0300

    Definindo timezons

--- js/time.js
+++ js/time.js
@@ -1,10 +1,14 @@
+const Timezone = Object.freeze({
+    SF: ["en-US", "America/Los_Angeles"],
+    POA: ["pt-BR", "America/Sao_Paulo"]
+});
+
 function timeSF() {
     let hour = new Date().getHours() - 4;
     let min = new Date().getMinutes();
     let minFormatt = ("00" + min).slice(-2);
 
     document.getElementById("timeSF").innerHTML = "Hora " + hour + "h" + minFormatt;
-
 }
 
 function timePOA() {
@@ -13,10 +17,14 @@ function timePOA() {
     let minFormatt = ("00" + min).slice(-2);
 
     document.getElementById("timePOA").innerHTML = "Hora " + hour + "h" + minFormatt;
+}
+
+function createTime(timezone) {
+    const [locale, timeZone] = timezone
+
+    let date = new Date().toLocaleString(, {timeZone: "America/Los_Angeles"})
 
 }
 
-(function() {
-    setInterval(function(){ timePOA() }, 1000);
-    setInterval(function(){ timeSF() }, 1000);
-})();
+setInterval(function(){ timePOA() }, 1000);
+setInterval(function(){ timeSF() }, 1000);
