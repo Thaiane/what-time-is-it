935d933ce339023efe15c90058dd8d31d1f1987f
Thaiane Braga <Thaiane_Braga@gap.com>
2020-09-03 19:10:49 -0300

    Arrumando a atualização automatica do horario

--- js/time.js
+++ js/time.js
@@ -1,7 +1,7 @@
-const Timezone = Object.freeze({
-    SF: ["en-US", "America/Los_Angeles"],
-    POA: ["pt-BR", "America/Sao_Paulo"]
-});
+// const Timezone = Object.freeze({
+//     SF: ["en-US", "America/Los_Angeles"],
+//     POA: ["pt-BR", "America/Sao_Paulo"]
+// });
 
 function timeSF() {
     let hour = new Date().getHours() - 4;
@@ -11,20 +11,17 @@ function timeSF() {
     document.getElementById("timeSF").innerHTML = "Hora " + hour + "h" + minFormatt;
 }
 
-function timePOA() {
-    let hour = new Date().getHours();
-    let min = new Date().getMinutes();
-    let minFormatt = ("00" + min).slice(-2);
+// function timePOA() {
+//     let hour = new Date().getHours();
+//     let min = new Date().getMinutes();
+//     let minFormatt = ("00" + min).slice(-2);
+//
+//     document.getElementById("timePOA").innerHTML = "Hora " + hour + "h" + minFormatt;
+// }
+//
+//
 
-    document.getElementById("timePOA").innerHTML = "Hora " + hour + "h" + minFormatt;
-}
-
-function createTime(timezone) {
-    const [locale, timeZone] = timezone
-
-    let date = new Date().toLocaleString(, {timeZone: "America/Los_Angeles"})
-
-}
-
-setInterval(function(){ timePOA() }, 1000);
-setInterval(function(){ timeSF() }, 1000);
+(function() {
+    setInterval(function(){ timePOA() }, 1000);
+    setInterval(function(){ timeSF() }, 1000);
+})();
