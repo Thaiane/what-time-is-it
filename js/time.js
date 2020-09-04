18f4f0c514344bd0cb940c9863fcc8ed8b4d0956
Thaiane Braga <Thaiane_Braga@gap.com>
2020-09-04 13:31:23 -0300

    Timezones diferentes <3

--- js/time.js
+++ js/time.js
@@ -3,18 +3,27 @@ const Timezone = Object.freeze({
     POA: ["pt-br", "America/Sao_Paulo"]
 });
 
-function createTime(timezone, fn) {
+function atualizarFusoPOA(time) {
+    document.getElementById("timePOA").innerHTML = "Hora: " + time;
+}
+
+function atualizarFusoSF(time) {
+    document.getElementById("timeSF").innerHTML = "Hora: " + time;
+}
+
+function createTime(timezone, atualizarFuso) {
     const [locale, timeZone] = timezone
 
-    let date = new Date().toLocaleTimeString(locale, {timeZone: "America/Los_Angeles"})
-    document.getElementById("timePOA").innerHTML = "Hora: " + date;
-    fn(date)
+    let time = new Date().toLocaleTimeString(locale, {timeZone: timeZone})
+    atualizarFuso(time)
 }
-// passar uma funcao para renderizar por lugar
 
+document.getElementById("atualizarFusoPOA").addEventListener("onclick", function() {
+    createTime(Timezone.POA, atualizarFusoPOA)
+});
+document.getElementById("atualizarFusoSF").addEventListener("onclick", function() {
+    createTime(Timezone.SF, atualizarFusoSF)
+});
 
-setInterval(function(){ createTime(Timezone.POA) }, 1000);
-setInterval(function(){ createTime(Timezone.SF) }, 1000);
-
-document.getElementById("atualizarFusoPOA").addEventListener("onclick", function() {createTime(Timezone.POA)})
-document.getElementById("atualizarFusoSF").addEventListener("onclick", function() {createTime(Timezone.SF)})
+setInterval(function(){ createTime(Timezone.POA, atualizarFusoPOA) }, 1000);
+setInterval(function(){ createTime(Timezone.SF, atualizarFusoSF) }, 1000);
