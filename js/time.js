d76a1618eab4a2a97678c77a99310beafd593df1
Thaiane Braga <Thaiane_Braga@gap.com>
2020-09-08 21:45:49 -0300

    Add animation

--- js/time.js
+++ js/time.js
@@ -59,3 +59,92 @@ fetch("https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D?lang=en&un
 .catch(err => {
 	console.log(err);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var
+
+w      = window.innerWidth,
+h      = window.innerHeight,
+canvas = document.querySelector('canvas');
+ctx    = canvas.getContext('2d'),
+rate   = 50,
+arc    = 500,
+time   = 0,
+count  = 0,
+size   = 2,
+speed  = 10,
+lights = [],
+colors = ['hsl(210 15% 10%)'],
+
+init = () => {
+  for (var i = 0; i < arc; i++) {
+    lights[i] = {
+      x: Math.ceil(Math.random() * w),
+      y: Math.ceil(Math.random() * h),
+      toX: Math.random() * 5 + 1,
+      toY: Math.random() * 5 + 1,
+      c: colors[Math.floor(Math.random() * colors.length)],
+      size: Math.random() * size
+    }
+  }
+},
+
+bubble = () => {
+  ctx.clearRect(0, 0, w, h);
+
+  for (var i = 0; i < arc; i++) {
+    var li = lights[i];
+
+    ctx.beginPath();
+    ctx.arc(li.x, li.y, li.size, 0, Math.PI * 2, false);
+    ctx.fillStyle = li.c;
+    ctx.fill();
+
+    li.x = li.x + li.toX * (time * 0.05);
+    li.y = li.y + li.toY * (time * 0.05);
+
+    if (li.x > w) li.x = 0;
+    if (li.y > h) li.y = 0;
+    if (li.x < 0) li.x = w;
+    if (li.y < 0) li.y = h;
+  }
+
+  if (time < speed) {
+    time++;
+  }
+
+  timerID = setTimeout(bubble, 1000 / rate);
+};
+
+canvas.setAttribute('width', w);
+canvas.setAttribute('height', h);
+
+init();
+
+bubble();
